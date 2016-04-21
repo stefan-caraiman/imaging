bd5eb5f8236047e657de1adaf23f7049287c9205
Stefan <scaraiman@cloudbasesolutions.com>
2016-04-21 13:43:27 +0000

    Add powershell files

---
+++ Logon.ps1
@@ -0,0 +1,28 @@
+$ErrorActionPreference = "Stop"
+
+try
+{
+    $Host.UI.RawUI.WindowTitle = "Winrm config..."
+    $winrmLUrl = "https://raw.github.com/PCManticore/windows-openstack-imaging-tools/master/SetupWinRMAccess.ps1"
+    $winrmPath = "$ENV:Temp\SetupWinRMAccess.ps1"
+    (new-object System.Net.WebClient).DownloadFile($winrmLUrl, $winrmPath)
+    powershell -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -File $winrmPath
+
+    $Host.UI.RawUI.WindowTitle = "Press any key to continue..."    
+    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
+
+    $Host.UI.RawUI.WindowTitle = "Running Sysprep..."        
+    $unattendXMLUrl = "https://raw.githubusercontent.com/PCManticore/windows-openstack-imaging-tools/master/Unattend.xml"
+    $unattendXMLPath = "$ENV:Temp\Unattend.xml"
+    (new-object System.Net.WebClient).DownloadFile($unattendXMLUrl, $unattendXMLPath)
+    & "$ENV:SystemRoot\System32\Sysprep\Sysprep.exe" `/generalize `/oobe `/shutdown `/unattend:"$unattendXMLPath"
+}
+catch
+{
+    $host.ui.WriteErrorLine($_.Exception.ToString())
+    $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    throw
+}
